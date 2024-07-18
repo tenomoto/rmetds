f991be0059d5e7eb052048b11fc3097a3465b577
eno <>
2024-07-18 07:40:27 +0000

    add gaqd
    FossilOrigin-Name: c51b3d3f2fdba514139491360fedab4108fcfb5ad823702068408c454fc79528

---
+++ gaus.R
@@ -0,0 +1,16 @@
+library(dotCall64)
+
+dyn.load("gaqd.so")
+
+gaqd <- function(nlat) {
+  w <- 0
+  lwork <- 0L
+  gaus <- .C64("gaqd",
+       c("integer", "double", "double", "double", "integer", "integer"),
+       nlat, theta = numeric_dc(nlat), wts = numeric_dc(nlat),
+       w, lwork, ierror= integer_dc(1),
+       INTENT=c("rw", "w", "w", "r", "r", "w"))
+  gaus$w <- NULL
+  gaus$lwork <- NULL
+  gaus[c(1, 2, 3, 6)]
+}
